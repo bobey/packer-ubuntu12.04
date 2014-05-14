8537a918386cb383cabaefbe077fc3bbd93e51b6
Olivier Balais <obalais@gmail.com>
2014-05-14 19:40:50 +0200

    Ajout post-process Vagrant
    - Changement utilisateur packer -> vagrant
    - Ajout script vagrant.sh pour configurer sudo nopasswd et clé ssh vagrant

---
+++ scripts/vagrant.sh
@@ -0,0 +1,13 @@
+// Variables
+VAGRANT_USER=vagrant
+VAGRANT_HOME=/home/$VAGRANT_USER
+VAGRANT_KEY_URL=https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+
+// Sudo
+echo "${VAGRANT_USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+// Vagrant ssh key
+mkdir $VAGRANT_HOME/.ssh
+cd $VAGRANT_HOME/.ssh
+wget --no-check-certificate "${VAGRANT_KEY_URL}" -O authorized_keys
+chown -R ${VAGRANT_USER}:${VAGRANT_USER}
