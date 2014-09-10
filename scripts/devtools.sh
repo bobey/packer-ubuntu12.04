eb2aa4b9d83d46b1a5c9a85b3d4cebd4662b1d76
Olivier Balais <obalais@gmail.com>
2014-09-10 16:25:16 +0200

    Ajout de mysql, git, curl, etc

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 apt-get update
-apt-get install python-software-properties -y
+apt-get install -y python-software-properties
+apt-get install -y software-properties-common
 
-# Installation de PHP 5.5
-add-apt-repository ppa:ondrej/php5
-apt-get install php5 -y
+# Installation de PHP 5.5 + MySql
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
+add-apt-repository -y ppa:ondrej/php5
+apt-get install -y php5 mysql-server
 
 # Installation de Ruby
-apt-get install ruby1.9.1-full -y
+apt-get install -y ruby1.9.1-full
 gem install bundler -y
+
+# Git
+apt-get install -y git
+
+# Divers
+apt-get install -y curl vim htop gcc make
